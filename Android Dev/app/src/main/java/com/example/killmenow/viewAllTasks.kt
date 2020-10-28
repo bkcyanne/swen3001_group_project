d52f5010677c42f77a5c6e70facbadec606e3dd5
Corey E <CoreyJ.Edwards24atgmail.com>
2020-10-28 13:44:31 -0500

    Added View all Task page, working on the basic layout, then adding functionality, then enhancing front end

---
+++ Android Dev/app/src/main/java/com/example/killmenow/viewAllTasks.kt
@@ -0,0 +1,11 @@
+package com.example.killmenow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class viewAllTasks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_all_tasks)
+    }
+}
