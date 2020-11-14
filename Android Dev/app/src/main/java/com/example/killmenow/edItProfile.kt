34cc6adc5ceef1537c4ed8191a1568695f6e9f4b
Corey E <CoreyJ.Edwards24atgmail.com>
2020-11-14 11:29:36 -0500

    Edit Profile page added here

---
+++ Android Dev/app/src/main/java/com/example/killmenow/edItProfile.kt
@@ -0,0 +1,11 @@
+package com.example.killmenow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class edItProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ed_it_profile)
+    }
+}
