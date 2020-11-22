cf2fecd1230054d483fbdf901dc440677fe327b7
Corey E <CoreyJ.Edwards24atgmail.com>
2020-11-22 15:33:11 -0500

    Finger print API added, functional on mainactivity

--- Android Dev/app/src/main/java/com/example/killmenow/MainActivity.kt
+++ Android Dev/app/src/main/java/com/example/killmenow/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.killmenow
 
 import android.content.Intent
+
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
+import androidx.annotation.NonNull
 
-import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.from
+import androidx.biometric.BiometricPrompt
+import android.hardware.biometrics.BiometricPrompt.AuthenticationCallback
+import androidx.biometric.BiometricPrompt.*
+import androidx.core.content.ContextCompat
+
 import androidx.lifecycle.ViewModelProvider
 import com.example.database.User
 import com.example.database.UserViewModel
@@ -21,9 +30,11 @@ import com.google.android.gms.common.ConnectionResult
 
 
 import java.lang.Thread.sleep
+import java.util.Date.from
+import java.util.concurrent.Executor
 
 
- class MainActivity() : AppCompatActivity(),GoogleApiClient.OnConnectionFailedListener{
+class MainActivity() : AppCompatActivity(),GoogleApiClient.OnConnectionFailedListener{
 
     lateinit var uN: String
     lateinit var pW: String
@@ -34,64 +45,147 @@ import java.lang.Thread.sleep
     lateinit var jumpStartGoogleSignInResult :GoogleSignInResult
      lateinit var createAccUserViewModel: UserViewModel
      lateinit var userList :List<User>
+     lateinit var fingerIcon:ImageView
+     lateinit var bioManager:BiometricManager
+    lateinit var bioPrompt:BiometricPrompt
+    lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    lateinit var executor: Executor
+
+     @RequiresApi(Build.VERSION_CODES.Q)
+
      override fun onCreate(savedInstanceState: Bundle?) {
 
-        createAccUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         createAccUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
 
 
-        uNInput = findViewById<EditText>(R.id.Username)
-        pWInput = findViewById<EditText>(R.id.Password)
+         uNInput = findViewById<EditText>(R.id.Username)
+         pWInput = findViewById<EditText>(R.id.Password)
 
-        for (x in 0 until 2) {
-            val loginAccButton = findViewById<Button>(R.id.loginButton)
-            loginAccButton.setOnClickListener {
-                uN = uNInput.text.toString()
-                pW = pWInput.text.toString()
-
-
-                    if(uN=="AdminCorey"&&pW=="tasteepatty"){
-
-                    Toast.makeText(this, "Welcome back $uN!", Toast.LENGTH_SHORT).show()
-                    sleep(3000)
+         for (x in 0 until 2) {
+             val loginAccButton = findViewById<Button>(R.id.loginButton)
+             loginAccButton.setOnClickListener {
+                 uN = uNInput.text.toString()
+                 pW = pWInput.text.toString()
 
 
-                    val intent = Intent(this, HomePage::class.java)
-                    startActivity(intent)
-                        finish()
+                 if (uN == "AdminCorey" && pW == "tasteepatty") {
 
-                } else {
-                    Toast.makeText(
-                        this,
-                        "Invalid Credentials, $counter attempts remaining",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    counter--
-                }
-            }
-        }
-        val noAccButton = findViewById<Button>(R.id.noAccButton)
-        noAccButton.setOnClickListener {
-            val intent = Intent(this, accCreationPage::class.java)
-            startActivity(intent)
-            finish()
-
-        }
-
-        val jumpStartGSO = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-        val jumpStartGoogleApiClient = GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(
-            Auth.GOOGLE_SIGN_IN_API,jumpStartGSO).build()
-
-        jumpStartSignInButton=findViewById<Button>(R.id.GoogleSignUp)
-        jumpStartSignInButton.setOnClickListener{
+                     Toast.makeText(this, "Welcome back $uN!", Toast.LENGTH_SHORT).show()
+                     sleep(3000)
 
 
-            val jumpStartGoogleIntent =Auth.GoogleSignInApi.getSignInIntent(jumpStartGoogleApiClient)
-            startActivityForResult(jumpStartGoogleIntent,SIGN_IN)
-        }
-    }
+                     val intent = Intent(this, HomePage::class.java)
+                     startActivity(intent)
+                     finish()
 
+                 } else {
+                     Toast.makeText(
+                         this,
+                         "Invalid Credentials, $counter attempts remaining",
+                         Toast.LENGTH_SHORT
+                     ).show()
+                     counter--
+                 }
+             }
+         }
+
+
+         val noAccButton = findViewById<Button>(R.id.noAccButton)
+         noAccButton.setOnClickListener {
+             val intent = Intent(this, accCreationPage::class.java)
+             startActivity(intent)
+             finish()
+
+         }
+
+         val jumpStartGSO =
+             GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+         val jumpStartGoogleApiClient =
+             GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(
+                 Auth.GOOGLE_SIGN_IN_API, jumpStartGSO
+             ).build()
+
+         jumpStartSignInButton = findViewById<Button>(R.id.GoogleSignUp)
+         jumpStartSignInButton.setOnClickListener {
+
+
+             val jumpStartGoogleIntent =
+                 Auth.GoogleSignInApi.getSignInIntent(jumpStartGoogleApiClient)
+             startActivityForResult(jumpStartGoogleIntent, SIGN_IN)
+         }
+
+         //fingerprint stuff here
+         fingerIcon = findViewById<ImageView>(R.id.FingereprintIcon)
+         bioManager = BiometricManager.from(this)
+
+             when (bioManager.canAuthenticate()) {
+                 BiometricManager.BIOMETRIC_SUCCESS -> {
+
+
+                 }
+
+
+                 BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                     Toast.makeText(this, "Sorry, Fingerprint is Unavailable", Toast.LENGTH_SHORT)
+                         .show()
+
+                 }
+                 BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                     Toast.makeText(
+                         this,
+                         "Add a fingerprint to your device in order to use this",
+                         Toast.LENGTH_SHORT
+                     ).show()
+                 }
+                 BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                     Toast.makeText(
+                         this,
+                         "Sorry,this system does not support Fingerprint Authentication",
+                         Toast.LENGTH_SHORT
+                     ).show()
+
+                 }
+                 else -> {
+                     Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+
+                 }
+             }
+             executor = ContextCompat.getMainExecutor(this)
+             bioPrompt = BiometricPrompt(this, executor, object :
+                 BiometricPrompt.AuthenticationCallback() {
+                 override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                     super.onAuthenticationError(errorCode, errString)
+                 }
+
+                 override fun onAuthenticationSucceeded(result: AuthenticationResult) {
+                     super.onAuthenticationSucceeded(result)
+                     Toast.makeText(this@MainActivity, "WE DID IT BOYZZZ", Toast.LENGTH_SHORT)
+                         .show()
+                     navToHomePage()
+                 }
+
+
+                 override fun onAuthenticationFailed() {
+                     super.onAuthenticationFailed()
+                 }
+
+             })
+
+            promptInfo =object:BiometricPrompt.PromptInfo.Builder(){}
+                .setTitle("Login")
+                .setDescription("Use your fingerprint to login")
+                .setNegativeButtonText("Cancel")
+                .build()
+
+
+         fingerIcon.setOnClickListener{
+
+             bioPrompt.authenticate(promptInfo)
+         }
+     }
 
 
 
@@ -120,5 +214,8 @@ import java.lang.Thread.sleep
          finish()
 
      }
+
+
+
  }
 
